4ef34cd885f576e2a3f0e942dd0328089f864d55
Emre Cenk <emrecenk9@gmail.com>
2021-06-19 13:29:04 -0400

    implemented a remove line function. created a testing directory

--- databse_interaction.py
+++ databse_interaction.py
@@ -2,10 +2,29 @@
 # for now I will be using a text file to emulate a database. Hopefully I won't need to create an actual remote databse
 # in the future.
 
-def parse_usernames(file_path, separator_character):
+def parse_usernames(file_path: str, separator_character: str = "\n"):
     file = open(file_path, "r")
-    text = file.read().split(separator_character)
+    username_list = file.read().split(separator_character)
     file.close()
 
-    return text
+    return username_list
 
+def remove_line(file_path: str, index: int, separator_character: str = "\n"):
+    username_list = parse_usernames(file_path, separator_character)
+    file = open(file_path, "w")
+
+    new_text = ""
+    try:
+        username_list.pop(index)
+    except IndexError:
+        print("IndexError at the 'remove_line' function in 'databse_inteaction.py'")
+        return
+    for i in range(len(username_list)):
+        new_text += str(username_list[i]) + "\n"
+
+    new_text = new_text[:-1]
+
+    file.write(new_text)
+    file.close()
+
+    return new_text
---
+++ tests/file_interaction_tests.py
@@ -0,0 +1,6 @@
+#testing removing a line from a file:
+
+from databse_interaction import remove_line
+
+ap = remove_line(r"C:\Users\Murat\Desktop\Emre\Python\InstaServer\pending_usernames.txt", 4)
+
